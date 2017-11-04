3f82e14a525cc5bd83068ad2682575696aa635bd
Lucas Moura <lucas.moura128@gmail.com>
2017-11-04 19:51:20 -0200

    Add tests to dataset formatter files

---
+++ dataset_formatter/tests/test_format_dataset.py
@@ -0,0 +1,20 @@
+import unittest
+
+from format_dataset import remove_html_from_text
+
+
+class FormatDatasetTest(unittest.TestCase):
+
+    def test_remove_html_from_text(self):
+        text_string1 = 'This is my review.<br /><br />I liked the movie.'
+        expected_string1 = 'This is my review. I liked the movie.'
+        actual_string1 = remove_html_from_text(text_string1)
+
+        self.assertEqual(expected_string1, actual_string1)
+
+        text_string2 = ('This is my review.<br /><br />I liked the movie.<br /><br />' +
+                        'I will watch it again')
+        expected_string2 = 'This is my review. I liked the movie. I will watch it again'
+        actual_string2 = remove_html_from_text(text_string2)
+
+        self.assertEqual(expected_string2, actual_string2)
