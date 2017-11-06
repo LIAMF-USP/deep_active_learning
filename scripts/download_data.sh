956c7039a85b92772d9d9737705cdd07a66b11ad
Lucas Moura <lucas.moura128@gmail.com>
2017-11-06 15:59:52 -0200

    Download GloVe files

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -8,6 +8,11 @@ set -e
 DATA_DIR="data/"
 DATASET_URL="http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 DATASET_DIR="aclImdb/"
+DATASET_TARBALL="aclImdb_v1.tar.gz"
+
+GLOVE_URL="http://nlp.stanford.edu/data/glove.6B.zip"
+GLOVE_ZIP="glove.6B.zip"
+GLOVE_FILE="glove.6B.50d.txt"
 
 if [ ! -d "$DATA_DIR" ]; then
     mkdir "$DATA_DIR"
@@ -17,10 +22,18 @@ cd "$DATA_DIR"
 
 if [ ! -d "$DATASET_DIR" ]; then
     wget "$DATASET_URL"
-    tar xvf aclImdb_v1.tar.gz
-    rm aclImdb_v1.tar.gz
+    tar xvf "$DATASET_TARBALL"
+    rm "$DATASET_TARBALL"
 else
     echo "Dataset already downloaded!"
+fi
+
+if [ ! -e "$GLOVE_FILE" ]; then
+    wget "$GLOVE_URL"
+    unzip "$GLOVE_ZIP" 
+    rm "$GLOVE_ZIP"
+else
+    echo "GloVe file already downloaded!"
 fi
 
 cd ..
