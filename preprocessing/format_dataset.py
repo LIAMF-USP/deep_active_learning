122cc3514a59bc2c6397d55e123ceae5dbd560db
Lucas Moura <lucas.moura128@gmail.com>
2017-11-06 15:31:37 -0200

    Add new string preprocessing step

--- preprocessing/format_dataset.py
+++ preprocessing/format_dataset.py
@@ -1,8 +1,13 @@
 import re
 
 
-def remove_html_from_text(review_text):
-    return re.sub(r'<br\s/><br\s/>', ' ', review_text)
+def remove_html_from_text(text):
+    return re.sub(r'<br\s/><br\s/>', ' ', text)
+
+
+def remove_special_characters_from_text(text):
+    text = re.sub(r"[^A-Za-z0-9(),!?\'\`]", ' ', text)
+    return re.sub(r'\s{2,}', ' ', text)
 
 
 def to_lower(review_text):
--- tests/test_format_dataset.py
+++ tests/test_format_dataset.py
@@ -1,6 +1,7 @@
 import unittest
 
-from preprocessing.format_dataset import remove_html_from_text
+from preprocessing.format_dataset import (remove_html_from_text,
+                                          remove_special_characters_from_text)
 
 
 class FormatDatasetTest(unittest.TestCase):
@@ -16,5 +17,18 @@ class FormatDatasetTest(unittest.TestCase):
                         'I will watch it again')
         expected_string2 = 'This is my review. I liked the movie. I will watch it again'
         actual_string2 = remove_html_from_text(text_string2)
+
+        self.assertEqual(expected_string2, actual_string2)
+
+    def test_remove_special_characters_from_text(self):
+        text_string1 = 'This movie is awesome!!!it is like a "poem"'
+        expected_string1 = 'This movie is awesome!!!it is like a poem '
+        actual_string1 = remove_special_characters_from_text(text_string1)
+
+        self.assertEqual(expected_string1, actual_string1)
+
+        text_string2 = 'I love this movie, but I ask, why?'
+        expected_string2 = 'I love this movie, but I ask, why?'
+        actual_string2 = remove_special_characters_from_text(text_string2)
 
         self.assertEqual(expected_string2, actual_string2)
