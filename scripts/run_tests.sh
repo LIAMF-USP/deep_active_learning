58b5838b67304a7e452d5dcd57589aefd052766f
Lucas Moura <lucas.moura128@gmail.com>
2017-11-07 13:57:09 -0200

    Add script to run tests

---
+++ scripts/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+#usage
+#./script/run_tests.sh $param
+#param=all  - Run all tests
+#param=slow - Run all slow tests
+#param=fast - Run fast tests (Default argument)
+
+PARAM=${1:-fast}
+
+if [ $PARAM == "fast" ]; then
+    echo "Running fast tests"
+    nose2 -A "!slow"
+elif [ $PARAM == "slow" ]; then
+    echo "Running slow tests"
+    nose2 -A slow
+elif [ $PARAM == "all" ]; then
+    echo "Running all tests"
+    nose2
+fi
