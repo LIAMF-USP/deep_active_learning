ce2c8c1bc118bea8273419a93faa5cacde57d6ad
Lucas Moura <lucas.moura128@gmail.com>
2017-11-10 15:55:57 -0200

    Add test data handling into script

--- scripts/run_dataset_preprocessing.sh
+++ scripts/run_dataset_preprocessing.sh
@@ -13,10 +13,18 @@ GLOVE_FILE="data/glove.6B.50d.txt"
 SENTENCE_SIZE=250
 OUTPUT_DIR="data/aclImdb_formatted"
 
+echo "Preprocessing training data..."
 python preprocess_dataset.py \
     --data-dir=${DATA_DIR} \
     --dataset-type=${DATASET_TRAIN} \
-    --glove-file=$GLOVE_FILE \
-    --sentence-size=$SENTENCE_SIZE \
+    --glove-file=${GLOVE_FILE} \
+    --sentence-size=${SENTENCE_SIZE} \
     --output-dir=${OUTPUT_DIR}
 
+echo -e "\n\nPreprocessing test data..."
+python preprocess_dataset.py \
+    --data-dir=${DATA_DIR} \
+    --dataset-type=${DATASET_TEST} \
+    --glove-file=${GLOVE_FILE} \
+    --sentence-size=${SENTENCE_SIZE} \
+    --output-dir=${OUTPUT_DIR}
